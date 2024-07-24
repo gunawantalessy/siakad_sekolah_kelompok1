bc0a4c6585d8d5e66c822fae3e020c1815728db8
gunawantalessy <gunawantalessy@gmail.com>
2024-07-24 14:10:54 +0900

    first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; {{ now()->year }} <div class="bullet"></div> Develop By <a href="http://kelompok1.classsic21.com/">KELOMPOK 1</a>
+    </div>
+    <div class="footer-right">
+        2.3.0
+    </div>
+</footer>
